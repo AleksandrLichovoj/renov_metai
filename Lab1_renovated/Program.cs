5724c56f6f9592ab21bde8af7db3c1290f52f5cd
cicuit <https://github.com/CriftProg/vgtu-app2>
2022-11-25 20:19:31 +0200

    reading_file

--- Lab1_renovated/Program.cs
+++ Lab1_renovated/Program.cs
@@ -1,11 +1,39 @@
 ﻿using CsvHelper;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Formats.Asn1;
 using System.Globalization;
 using System.Net.NetworkInformation;
 
-var path = @"C:\\Users\\cicui\\Documents\\apartment_buildings_2019.csv";
+var path = @"C:\Users\cicui\Documents\\apartment_buildings_2019.csv";
+using (TextFieldParser csvParser = new TextFieldParser(path))
+{
+    csvParser.SetDelimiters(new string[] { ";" });
+    csvParser.ReadLine();
 
+    while (!csvParser.EndOfData)
+    {
+        string[] sarasas = csvParser.ReadFields();
+        string Id = sarasas[0];
+        string adresas = sarasas[1];
+        string namo_valdytojas = sarasas[2];
+        string valdymo_forma = sarasas[3];
+        string paskyrimo_pagrindas = sarasas[4];
+        string administratoriaus_pabaigos_Data = sarasas[5];
+        string paskirtis = sarasas[6];
+        string uni_nr = sarasas[7];
+        string bendr_plotas = sarasas[8];
+        string naud_plotas = sarasas[9];
+        string build_year = sarasas[10];
+        string renov_metai = sarasas[11];
+        string renovacijos_statusas = sarasas[12];
+        string energ_naudingumo_klase = sarasas[13];
+        string butu_skaicius = sarasas[14];
+        string negyvenamuju_palapu_skaicius = sarasas[15];
+        string korpusas = sarasas[16];
+        string sklypo_plotas = sarasas[17];
+    }
+}
 public class Namas
 {
     public int Id { get; set; }
@@ -18,6 +46,7 @@ public class Namas
     public string uni_nr { get; set; }
     public int bendr_plotas { get; set; }
     public int naud_plotas { get; set; }
+    public DateTime build_year { get; set; }
     public DateTime renov_metai { get; set; }
     public string renovacijos_statusas { get; set; } 
     public string energ_naudingumo_klase { get; set; }
