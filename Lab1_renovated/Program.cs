b55529f3e89460b77b1b9ccf3ff0686cca2e69cf
cicuit <https://github.com/CriftProg/vgtu-app2>
2022-12-05 14:10:08 +0200

    need to fix "DisplayInConsole(buckets);" in line 93

--- Lab1_renovated/Program.cs
+++ Lab1_renovated/Program.cs
@@ -5,12 +5,18 @@ using System.Formats.Asn1;
 using System.Globalization;
 using System.Net.NetworkInformation;
 using System.Collections.Generic;
+using System.Net;
+using System.Xml.Linq;
 
+static void Main(string[] args) { 
 var path = @"C:\Users\cicui\Documents\\apartment_buildings_2019.csv"; //change path if needed
 int max_metai = 0;
 int min_metai = 2022;
 int yearsort = 2;
-string[] renov = null;
+int r = 0;
+int s = 0;
+string[] renov=null;
+Dictionary<int, Namas> namai = new Dictionary<int, Namas>();
 using (TextFieldParser csvParser = new TextFieldParser(path))
 {
     csvParser.SetDelimiters(new string[] { ";" });
@@ -37,11 +43,25 @@ using (TextFieldParser csvParser = new TextFieldParser(path))
         string negyvenamuju_palapu_skaicius = sarasas[15];
         string korpusas = sarasas[16];
         string sklypo_plotas = sarasas[17];
-        
+        s = s + 1;
+        int.TryParse(Id, out int id);
+        int.TryParse(bendr_plotas, out int bendras_plotas);
+        int.TryParse(naud_plotas, out int naudotas_plotas);
+        int.TryParse(butu_skaicius, out int butu_skaicius_name);
+        int.TryParse(negyvenamuju_palapu_skaicius, out int tusciu_butu_skaicius);
+        int.TryParse(sklypo_plotas, out int sklypo_bendrasisPlotas);
+
+        DateTime.TryParse(administratoriaus_pabaigos_Data, out DateTime pabaigos_data);
+        DateTime.TryParse(build_year, out DateTime pastatymo_metai);
+        DateTime.TryParse(renov_metai, out DateTime renovacijos_metai);
+
         if (renovacijos_statusas == "Renovuotas")
         {
+
             renov = sarasas;
-            try { 
+            AddToDictionary(namai, id, adresas, namo_valdytojas, valdymo_forma, paskyrimo_pagrindas, pabaigos_data, paskirtis, uni_nr, bendras_plotas, naudotas_plotas, pastatymo_metai, renovacijos_metai, renovacijos_statusas, energ_naudingumo_klase, butu_skaicius_name, tusciu_butu_skaicius, korpusas, sklypo_bendrasisPlotas);
+            try
+            {
                 int metai_num = Int32.Parse(renov[11]);
                 if (metai_num > max_metai)
                     max_metai = metai_num;
@@ -52,21 +72,71 @@ using (TextFieldParser csvParser = new TextFieldParser(path))
             {
                 Console.WriteLine($"Unable to parse '{renov[11]}'");
             }
+            r = r + 1;
         }
     }
-    int bucket_num = ((max_metai - min_metai)/yearsort)+1;
-    List<string>[] buckets = new List<string>[bucket_num];
-    for (int i = 0; i < bucket_num; i++)
-    {
-        buckets[i] = new List<string>();
-    }
 
-    for (int i = 0; i < renov.Length; i++)
-    {
-        int bucket = ((Int32.Parse(renov[11])-2000)/yearsort);
+            int bucket_num = ((max_metai - min_metai) / yearsort) + 1;
+            List<string>[] buckets = new List<string>[bucket_num];
+            for (int i = 0; i < bucket_num; i++)
+            {
+                buckets[i] = new List<string>();
+            }
+        
+            for (int i = 0; i < renov.Length; i++)
+            {
+                int bucket = ((Int32.Parse(renov[11]) - 2000) / yearsort);
         buckets[bucket].AddRange(renov);
+            
+        }
+
+        //DisplayInConsole(buckets);
+
     }
- 
+    
+}
+static void DisplayInConsole(string[] buckets)
+{
+    Console.WriteLine("---START WRITING---");
+    Console.WriteLine(buckets[1]);
+    foreach (string bucket in buckets)
+    {
+        for (int i = 0; i < bucket.Length; i++)
+        {
+            Console.WriteLine(bucket[i]);
+        }
+    }
+    Console.WriteLine("");
+    Console.WriteLine("---END WRITING---");
+
+
+}
+static void AddToDictionary(Dictionary<int, Namas> elements, int Id, string address, string owner, string ownershipForm, string appointmentBasis, DateTime adminstrationEndDate, string purpose, string uniNumber, int generalSize, int usedSize, DateTime buildYear, DateTime renovationYear, string renovationStatus, string energyClass, int houseCount, int emtyHouseCount, string corpus, int areaSize)
+{
+    Namas theElement = new Namas();
+
+    theElement.Id = Id;
+    theElement.adresas = address;
+    theElement.namo_valdytojas = owner;
+    theElement.valdymo_forma = ownershipForm;
+    theElement.paskyrimo_pagrindas = appointmentBasis;
+    theElement.administratoriaus_pabaigos_Data = adminstrationEndDate;
+    theElement.uni_nr = uniNumber;
+    theElement.bendr_plotas = generalSize;
+    theElement.naud_plotas = usedSize;
+    theElement.build_year = buildYear;
+    theElement.renov_metai = renovationYear;
+    theElement.renovacijos_statusas = renovationStatus;
+    theElement.energ_naudingumo_klase = energyClass;
+    theElement.butu_skaicius = houseCount;
+    theElement.negyvenamuju_palapu_skaicius = emtyHouseCount;
+    theElement.korpusas = corpus;
+    theElement.sklypo_plotas = areaSize;
+
+
+
+    elements.Add(key: theElement.Id, value: theElement);
+
 }
 public class Namas
 {
@@ -89,3 +159,5 @@ public class Namas
     public string korpusas { get; set; }
     public int sklypo_plotas { get; set; }
 }
+
+
